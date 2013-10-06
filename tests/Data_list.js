b413058fcfea3eb9f691f23ec25f7dbf970312f4
Ann Petrenko <ann.petrenko@gmail.com>
2013-10-06 21:21:37 +0400

    hidden

---
+++ tests/Data_list.js
@@ -0,0 +1,37 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: 4ayka
+ * Date: 03.10.13
+ * Time: 18:52
+ * To change this template use File | Settings | File Templates.
+ */
+$(function(){
+Data = {
+    form1 : {
+        a1 : {
+            name : "aAAA",
+            checked : "foo",
+            unchecked : "bar"
+        },
+        a2:
+        {
+            name : "e333333aAAsdfsd123123123A",
+            checked : "f124532453123oo",
+            unchecked : "b1ewrwqre23123ar"
+        },
+        a3:
+        {
+            name : "45665dfgsdgfaAA123123123A",
+            checked : "fsdfsdgf123123oo",
+            unchecked : "b1sdgfsdgf23123ar"
+        },
+        a4:
+        {
+            name : "kjhgv,mnbvaAA123123123A",
+            checked : "f1vcvb23123oo",
+            unchecked : "b1cvbcvb23123ar"
+        }
+    }
+};
+});
+
